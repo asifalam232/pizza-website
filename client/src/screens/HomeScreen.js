e287a00567ee85a142eecf85823ae0cc24b026ff
Asif Alam <alamtheboss232@gmail.com>
2022-08-28 23:43:41 +0100

    Add pizza route & node react connection

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React,{useEffect} from "react";
 import {Container, Row, Col} from "react-bootstrap";
-import AllPizza from "../pizza-data";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllPizzas} from "../actions/pizzaAction";
 import Pizza from "../components/Pizza";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+    const pizzaState = useSelector(state => state.getAllPizzaReducer);
+    const {loading, pizzas, error} = pizzaState;
+    useEffect(() => {
+        dispatch(getAllPizzas())
+    }, [dispatch]);
     return (
         <>
             <Container>
-                <Row>
-                    { AllPizza.map(pizza => (
-                        <Col md={4}>
-                            <Pizza pizza={pizza}/>
-                        </Col>
-                    )) }
-                </Row>
+                {
+                    loading ? (<h1>Loading ...</h1>)
+                        : error ? (<h1>Error while fetching pizzas</h1>)
+                        : (
+                                <Row>
+                                    { pizzas.map(pizza => (
+                                        <Col md={4}>
+                                            <Pizza pizza={pizza}/>
+                                        </Col>
+                                    )) }
+                                </Row>
+                            )
+                }
             </Container>
         </>
     );
