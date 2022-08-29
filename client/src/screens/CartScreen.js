f43d9d0faf6a3d925985ed110193ec2cb92b3440
Asif Alam <alamtheboss232@gmail.com>
2022-08-29 11:52:38 +0100

    Add logic for updating cart quantity

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import {Container, Row, Col} from "react-bootstrap";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import {FaMinusCircle, FaPlusCircle} from "react-icons/fa";
+import {addToCart} from "../actions/cartAction";
 
 const CartScreen = () => {
     const cartState = useSelector(state => state.cartReducer);
     const cartItems = cartState.cartItems;
+    const dispatch = useDispatch();
+
     return (
         <>
             <Container>
@@ -24,9 +27,25 @@ const CartScreen = () => {
                                             <h6>Price : {item.quantity} x {item.prices[0][item.variant]} = {item.price}</h6>
                                             <h6>
                                                 Quantity : &nbsp;
-                                                <FaMinusCircle className={"text-danger"}/> &nbsp;
+                                                <FaMinusCircle
+                                                    className={"text-danger"}
+                                                    style={{cursor:"pointer"}}
+                                                    onClick={() => {
+                                                        dispatch(
+                                                            addToCart(item,item.quantity-1, item.variant)
+                                                        );
+                                                    }}
+                                                /> &nbsp;
                                                 {item.quantity} &nbsp;
-                                                <FaPlusCircle className={"text-success"}/>
+                                                <FaPlusCircle
+                                                    className={"text-success"}
+                                                    style={{cursor:"pointer"}}
+                                                    onClick={() => {
+                                                        dispatch(
+                                                            addToCart(item,item.quantity+1, item.variant)
+                                                        );
+                                                    }}
+                                                />
                                             </h6>
                                         </Col>
                                         <Col md={5}>
