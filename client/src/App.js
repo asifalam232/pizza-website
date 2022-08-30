0214991c44211ad4139aa2998600a7c22059a26c
Asif Alam <alamtheboss232@gmail.com>
2022-08-30 20:53:25 +0100

    Create Contact Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <TopBar />
       <Switch>
         <Route path={"/about"} component={About} exact></Route>
+        <Route path={"/contact"} component={Contact} exact></Route>
       </Switch>
     </BrowserRouter>
   );
