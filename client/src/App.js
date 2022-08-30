44afaab711fca12192316fa9ee35406e2920a60a
Asif Alam <alamtheboss232@gmail.com>
2022-08-30 20:48:11 +0100

    Add about page again

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Container } from "react-bootstrap";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 import TopBar from "./components/TopBar";
+import About from "./components/About";
 
 function App() {
   return (
     <BrowserRouter>
       <TopBar />
+      <Switch>
+        <Route path={"/about"} component={About} exact></Route>
+      </Switch>
     </BrowserRouter>
   );
 }
