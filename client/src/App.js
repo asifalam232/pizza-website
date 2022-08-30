8d69fbdb85863d2838804813cdabb59fd89f446e
Asif Alam <alamtheboss232@gmail.com>
2022-08-30 21:14:18 +0100

    Add NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { Container } from "react-bootstrap";
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Policy from "./components/Policy";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <TopBar />
+      <NavBar />
       <Switch>
         <Route path={"/about"} component={About} exact></Route>
         <Route path={"/contact"} component={Contact} exact></Route>
