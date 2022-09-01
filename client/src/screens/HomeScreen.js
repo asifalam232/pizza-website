a7fa0e7319784e6a366ac4a9367a2025568d22ab
Asif Alam <alamtheboss232@gmail.com>
2022-09-01 21:52:38 +0100

    Fetch Pizza data from server

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AllPizza from "../pizza-data";
+import { useDispatch, useSelector } from "react-redux";
 import { Container, Row, Col } from "react-bootstrap";
+import { getAllPizzas } from "../actions/pizzaAction";
 import Pizza from "../components/Pizza";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const pizzastate = useSelector((state) => state.getAllPizzaReducer);
+  const { loading, pizzas, error } = pizzastate;
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, [dispatch]);
   return (
     <>
       <Container>
-        <Row>
-          {AllPizza.map((pizza) => (
-            <Col md={4}>
-              <Pizza pizza={pizza} />
-            </Col>
-          ))}
-        </Row>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <h1>Error while fetching pizzas</h1>
+        ) : (
+          <Row>
+            {pizzas.map((pizza) => (
+              <Col md={4}>
+                <Pizza pizza={pizza} />
+              </Col>
+            ))}
+          </Row>
+        )}
       </Container>
     </>
   );
