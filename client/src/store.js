aa565eac36b87f2471148bdc999c4cd06334a3ee
Asif Alam <alamtheboss232@gmail.com>
2022-09-02 21:36:48 +0100

    Add Login Form

--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { getAllPizzaReducer } from "./reducers/pizzaReducer";
 import { cartReducer } from "./reducers/cartReducer";
 import { registerUserReducer } from "./reducers/userReducer";
+import { loginUserReducer } from "./reducers/userReducer";
 
 const rootReducer = combineReducers({
   getAllPizzaReducer: getAllPizzaReducer,
   cartReducer: cartReducer,
   registerUserReducer: registerUserReducer,
+  loginUserReducer: loginUserReducer,
 });
+
+const currentUser = localStorage.getItem("currentItem")
+  ? JSON.parse(localStorage.getItem("currentUser"))
+  : null;
 
 const cartItems = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
@@ -18,6 +24,9 @@ const cartItems = localStorage.getItem("cartItems")
 const initialState = {
   cartReducer: {
     cartItems: cartItems,
+  },
+  loginUserReducer: {
+    currentUser: currentUser,
   },
 };
 const middleware = [thunk];
